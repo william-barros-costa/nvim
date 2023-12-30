65b4837b9794ee6e62c3403e779847983579dba9
william-barros-costa <william.de.barros.da.costa@gmail.com>
2023-12-30 20:02:10 +0000

    Now properly converts to markdown url

--- lua/kane/paste.lua
+++ lua/kane/paste.lua
@@ -1,16 +1,34 @@
 function get_clipboard_url()
-	local command_output = vim.fn.system('xclip -o -selection clipboard -t text/html | grep -oP \'(?<=href=")[^"]*\'')
 	return string.gsub(command_output, '\n', '')
 end
 
+function remove_html_tags(html)
+-- Remove all HTML tags except for <a> tags
+  local cleaned_html = string.gsub(html, "<([^>]+)>", function(match)
+    if string.match(match, "^a%s") then
+	return string.gsub(match, "a%s+href=[\"'](.-)[\"'][^>]*", "[%1][")
+    elseif string.match(match, "^/a%s*") then
+	return "]"
+    else
+      return ""    -- Remove other tags
+    end
+  end)
+
+  -- Decode HTML entities (optional)
+  cleaned_html = cleaned_html:gsub("&lt;", "<"):gsub("&gt;", ">"):gsub("&amp;", "&")
+
+  -- Remove unnecessary spaces
+  cleaned_html = cleaned_html:gsub("%s+", " ")
+
+  return cleaned_html
+end
 
 function paste_markdown_url()
 	local filetype = vim.bo.filetype
-	print(filetype)
 	if filetype == 'markdown' or filetype == 'mkd' or filetype == 'md' or filetype == 'vimwiki' then
-		local url = get_clipboard_url()
-		if url ~= '' then
-			vim .fn.setreg('+', string.format('[%s](%s)', url, url))
+		local clipboard_content = vim.fn.system('xclip -o -selection clipboard -t text/html')
+		if clipboard_content ~= '' then
+			vim.fn.setreg('+', remove_html_tags(clipboard_content))
 			vim.cmd('normal! "+p')
 		else
 			print('Clipboard does not contain a valid URL')
