eaa4508cdc87a147e0248f6debbef09a89b987dc
william-barros-costa <william.de.barros.da.costa@gmail.com>
2023-12-31 10:12:15 +0000

    Changed tab to 4 spaces

--- init.lua
+++ init.lua
@@ -6,3 +6,8 @@ vim.g.lightline = 'shades_of_purple'
 vim.o.compatible=false
 vim.cmd('filetype plugin on')
 vim.cmd('syntax on')
+
+vim.cmd('set expandtab')
+vim.cmd('set tabstop=4')
+vim.cmd('set softtabstop=4')
+vim.cmd('set shiftwidth=4')
