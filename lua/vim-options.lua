cb9c1f0a6b0574809971f438ecfcca510b907b44
william-barros-costa <william.de.barros.da.costa@gmail.com>
2024-01-08 11:53:46 +0000

    removed auto select completion

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,15 +2,21 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set clipboard^=unnamed,unnamedplus")
 vim.cmd("set number relativenumber")
 vim.g.mapleader = " "
+vim.g.mdip_imgdir = "img"
 
 -- Saves when Leaving insert
 vim.api.nvim_command([[autocmd InsertLeave * :silent! wa]])
+vim.api.nvim_command(
+  [[autocmd FileType markdown nmap <buffer><silent> <leader>p :call mdip#MarkdownClipboardImage()<CR>]]
+)
 
 -- Changing key binds
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', '<esc>', '<cmd>noh<cr>')
-vim.keymap.set('n', 'H', '<cmd>bp<cr>')
-vim.keymap.set('n', 'L', '<cmd>bn<cr>')
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-b>", "<C-v>")
+
+vim.keymap.set("n", "H", "<cmd>bp<cr>")
+vim.keymap.set("n", "L", "<cmd>bn<cr>")
--- readme.md
+++ readme.md
@@ -19,3 +19,5 @@ nvim .
 1. Use Netrw to go to .config/nvim/lua/kane/packer.lua
 2. Run `:so`
 3. Run `:PackerInstall`
+
+
