0b7025326f6e0a84c79a5b54082280d0987eb6a2
william-barros-costa <william.de.barros.da.costa@gmail.com>
2024-01-11 14:23:05 +0000

    More Changes

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,7 +10,6 @@ return {
         null_ls.builtins.formatting.beautysh,
         null_ls.builtins.formatting.isort,
         null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.completion.spell,
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
