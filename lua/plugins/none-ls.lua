5f8bb1175044329c463f58ba5df8157f2ebb6a3f
william-barros-costa <william.de.barros.da.costa@gmail.com>
2024-08-21 17:26:45 +0000

    Format on Save

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,6 +2,7 @@ return {
   "nvimtools/none-ls.nvim",
   config = function()
     local null_ls = require("null-ls")
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
@@ -13,6 +14,18 @@ return {
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.goimports_reviser,
       },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
     })
     vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
   end,
