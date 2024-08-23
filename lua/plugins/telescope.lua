0741b0c5f09c3552ab301af7f410f8e4c629ef1e
william-barros-costa <william.de.barros.da.costa@gmail.com>
2024-08-23 21:48:05 +0000

    added shortcut to roam between document symbols

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
       vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+      vim.keymap.set('n', '<leader>co', '<cmd>Telescope lsp_document_symbols<CR>', {})
       vim.keymap.set('n', '<leader>ps', function ()
         builtin.grep_string({ search = vim.fn.input("Grep > ") });
       end)
