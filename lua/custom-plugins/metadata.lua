bc7eb841978e690e80e7380db9dbe3f141eb2daa
william-barros-costa <william.de.barros.da.costa@gmail.com>
2024-08-26 14:23:37 +0000

    Adding type picket to yaml files (Ansible,Docker or Terraform)

--- lua/custom-plugins/metadata.lua
+++ lua/custom-plugins/metadata.lua
@@ -7,6 +7,36 @@ tags:
 
 ]]
 
+buf_enter_triggered = false
+
+local function get_yaml_type(callback)
+  local pickers = require("telescope.pickers")
+  local sorters = require("telescope.sorters")
+  local finders = require("telescope.finders")
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+  pickers.new({}, {
+    prompt_title = "Question",
+    finder = finders.new_table({
+      results = { "ansible", "terraform", "docker" },
+    }),
+    sorter = sorters.get_generic_fuzzy_sorter({}),
+    attach_mappings = function(prompt_bufnr, map)
+      map('n', '<esc>', function ()
+        actions.close(prompt_bufnr)
+        buf_enter_triggered = false
+      end)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        callback(selection.value)
+        buf_enter_triggered = false
+      end)
+      return true
+    end,
+  }):find()
+end
+
 local function get_filetype()
   local filetype = vim.bo.filetype
   if filetype == "vimwiki" then
@@ -46,14 +76,30 @@ local function set_tab_target()
   end, { noremap = true, silent = true })
 end
 
+local function insert_metadata(metadata_string)
+  write_metadata(metadata_string)
+  local tag_line, tag_length = 4, ("tag:  "):len()
+  change_cursor_position(tag_line, tag_length)
+  enter_insert_mode()
+  set_tab_target()
+end
+
 local function check_metadata()
+  if buf_enter_triggered == true then
+    return
+  end
+  buf_enter_triggered = true
   if needs_metadata() then
-    local metadata_string = string.format(metadata, get_filetype(), get_title())
-    write_metadata(metadata_string)
-    local tag_line, tag_length = 4, ("tag:  "):len()
-    change_cursor_position(tag_line, tag_length)
-    enter_insert_mode()
-    set_tab_target()
+    local filetype = get_filetype()
+    if filetype == "md" then
+      insert_metadata(string.format(metadata, filetype, get_title()))
+    else
+      get_yaml_type(function(type)
+        insert_metadata(string.format(metadata, type, get_title()))
+      end)
+    end
+  else
+    buf_enter_triggered = false
   end
 end
 
