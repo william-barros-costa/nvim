289f8b66fc1d6e23794ab32a6ed79e9f68c4ffe5
william-barros-costa <william.de.barros.da.costa@gmail.com>
2024-08-28 17:54:02 +0000

    added dap for go

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,29 +1,62 @@
 return {
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"rcarriga/nvim-dap-ui",
-		},
-		config = function()
-			local dap = require("dap")
-			local dapui = require("dapui")
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
 
-			dapui.setup()
+      dapui.setup()
 
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.open()
-			end
-			vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
-			vim.keymap.set("n", "<leader>dc", dap.continue, {})
-		end,
-	},
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.open()
+      end
+      vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+      vim.keymap.set("n", "<leader>dc", dap.continue, {})
+      vim.keymap.set("n", "<leader>dc", dap.continue, {})
+
+      dap.adapters.delve = {
+        type = 'server',
+        port = '${port}',
+        executable = {
+          command = 'dlv',
+          args = { 'dap', '-l', '127.0.0.1:${port}' },
+        }
+      }
+
+      dap.configurations.go = {
+        {
+          type = "delve",
+          name = "Debug",
+          request = "launch",
+          program = "${file}"
+        },
+        {
+          type = "delve",
+          name = "Debug Test",
+          request = "launch",
+          mode = "test",
+          program = "${file}"
+        },
+        {
+          type = "delve",
+          name = "Debug Test (go.mod)",
+          request = "launch",
+          mode = "test",
+          program = "./${relativeFileDirname}"
+        },
+      }
+    end,
+  },
 }
