7556cb1fb1567485a007197cd52f80d498be4236
william-barros-costa <william.de.barros.da.costa@gmail.com>
2024-08-28 20:18:47 +0000

    keymaps for dap

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -23,8 +23,18 @@ return {
         dapui.open()
       end
       vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+      vim.keymap.set("n", "<leader>dta", dap.clear_breakpoints, {})
       vim.keymap.set("n", "<leader>dc", dap.continue, {})
-      vim.keymap.set("n", "<leader>dc", dap.continue, {})
+      vim.keymap.set("n", "<leader>dn", dap.step_over, {})
+      vim.keymap.set("n", "<leader>di", dap.step_into, {})
+      vim.keymap.set("n", "<leader>do", dap.step_out, {})
+      vim.keymap.set("n", "<leader>ds", function ()
+        dap.terminate()
+        dapui.close()
+      end, {})
+      vim.keymap.set("n", "<leader>dr", dap.repl.open, {})
+      vim.keymap.set("n", "<leader>du", dap.up, {})
+      vim.keymap.set("n", "<leader>dd", dap.down, {})
 
       dap.adapters.delve = {
         type = 'server',
