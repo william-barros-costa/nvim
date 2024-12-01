c8089024c5f82bff5a59e7645ac00b5464dd38e4
william-barros-costa <william.de.barros.da.costa@gmail.com>
2024-12-01 23:06:08 +0000

    removed image.nvim

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -23,7 +23,6 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
-		"3rd/image.nvim",
 	},
 	config = function()
 		require("neo-tree").setup({
